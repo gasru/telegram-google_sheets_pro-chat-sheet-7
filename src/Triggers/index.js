e8e41542416d86a8374c2b254944b5fdb6757975
Alexander Ivanov <oshli.a.er@gmail.com>
2024-01-03 22:03:06 +0300

    update menu
    Signed-off-by: Alexander Ivanov <oshli.a.er@gmail.com>

--- src/Triggers/index.js
+++ src/Triggers/index.js
@@ -13,11 +13,24 @@ function onOpen() {
 
 /* exported userAvtionHiMessage */
 function userActionHiMessage() {
-  const message = ```
-Чтобы создать новый лист, выберите любой незаблокированный, поттом передите в меню
-
-Вы можете добавлять код Apps Script при необходимости
-  ```;
+  const message = `
+<style>
+  * {
+    font-family: Google Sans,Roboto,RobotoDraft,Helvetica,Arial,sans-serif;
+  }
+</style>
+<h2>Добро пожаловать в Таблицу чата!</h2>
+<div>
+  <p>Чтобы создать новый лист, выберите любой незаблокированный, поттом передите в меню</p>
+  <p>Вы можете добавлять код Apps Script при необходимости</p>
+</div>
+<div>
+  <p>
+    <a href="https://t.me/+pLLUBtcXIqY0MGMy" target="_blank">Наш чат</a> и 
+    <a href="https://t.me/+lmannExYEyg5OTZi" target="_blank">наш канал</a>
+  </p>
+</div>
+`;
 
   SpreadsheetApp.getUi().showModalDialog(HtmlService.createHtmlOutput(message), 'Приветствие');
 }
