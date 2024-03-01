830c60083db829e71e05da88c75fb01fd45bf1f8
Alexander Ivanov <oshli.a.er@gmail.com>
2024-03-01 13:38:47 +0300

    feat: add dynamic menu
    Signed-off-by: Alexander Ivanov <oshli.a.er@gmail.com>

--- src/Triggers/index.js
+++ src/Triggers/index.js
@@ -8,6 +8,7 @@ function onOpen() {
     .addSubMenu(
       SpreadsheetApp.getUi().createMenu('Примеры и вопросы').addItem('Копирование строк', userActionSample.name),
     )
+    .addSubMenu(getDynamicMenu_(SpreadsheetApp.getUi(), 'Динамическое меню'))
     .addToUi();
 }
 
---
+++ src/Меню с параметрами/inedx.js
@@ -0,0 +1,60 @@
+/**
+ * @typedef {{
+ *   caption: string,
+ *   functionName: string
+ * }} GSRU.DynamicMenu
+ */
+
+const DYNAMIC_MENU_CONFIG = [
+  {
+    caption: 'Пункт меню 1',
+    functionName: 'dynamicMenuItem',
+  },
+  {
+    caption: 'Пункт меню 2',
+    functionName: 'dynamicMenuItem',
+  },
+  {
+    caption: 'Пункт меню 3',
+    functionName: 'dynamicMenuItem',
+  },
+];
+
+/* exported getDynamicMenu_ */
+/**
+ *
+ * @param {GoogleAppsScript.Base.Ui} ui
+ * @param {string} name
+ */
+function getDynamicMenu_(ui, name) {
+  var menu = ui.createMenu(name);
+
+  configDynamicMenu_(menu, DYNAMIC_MENU_CONFIG);
+
+  return menu;
+}
+
+/**
+ * @param {GoogleAppsScript.Base.Menu} menu
+ * @param {GSRU.DynamicMenu[]} config
+ */
+function configDynamicMenu_(menu, config) {
+  config.forEach(function (item, i) {
+    menu.addItem(item.caption, item.functionName + i);
+  });
+}
+
+/* exported dynamicMenuItem */
+function dynamicMenuItem(e) {
+  var caption = e.item.caption;
+  var order = e.order;
+  Browser.msgBox(Utilities.formatString('Был нажат %sй пункт меню: %s', order + 1, caption));
+}
+
+(function (self) {
+  DYNAMIC_MENU_CONFIG.forEach(function (item, i) {
+    self[item.functionName + i] = function () {
+      return self[item.functionName]({ item: item, order: i });
+    };
+  });
+})(this);
