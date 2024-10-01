5553fa345107f30cdc2f285c4c705b368fb62d43
Alexander Ivanov <oshli.a.er@gmail.com>
2024-10-01 06:06:06 +0300

    feat: update for sheet-026
    Signed-off-by: Alexander Ivanov <oshli.a.er@gmail.com>

--- src/mergeSheets.js
+++ src/mergeSheets.js
@@ -1,3 +1,50 @@
+function triggerOnEdit(e) {
+  // function onEdit(e) {
+  let wb = SpreadsheetApp.getActive().getSheetByName('Get-Запрос'); //вот здесь нужно поменять название листа
+  var sheet = e.range.getSheet();
+
+  // Проверка, что изменения происходят на листе "1979"
+  if (sheet.getName() === 'Get-Запрос') {
+    //вот здесь нужно поменять название листа
+    var range = e.range;
+    // Проверка, что изменение произошло в колонке 6 (например, выпадающее меню в колонке F)
+    if (range.getColumn() == 5) {
+      //вот тут нужно поменять номер столбца со списком
+      var newValue = range.getValue();
+      // Если выбрано значение "Archive"
+      if (newValue === 'Archive') {
+        var valueA = sheet.getRange(range.getRow(), 1).getValue(); // Значение из колонки A текущей строки
+        var valueE = sheet.getRange(range.getRow(), 5).getValue(); // Значение из колонки E текущей строки
+
+        // Вывод значений valueA и valueE на экран
+        var ui = SpreadsheetApp.getUi();
+        //var response = ui.alert('Архивация', 'Значение из A: ' + valueA + '\nЗначение из E: ' + valueE, ui.ButtonSet.YES_NO);
+
+        // Логирование значений в консоль
+        console.log('Значение в колонке A:', valueA);
+        console.log('Значение в колонке E:', valueE);
+        wb.getRange('I2').setValue(valueA); //поменять ячейку для записи
+        wb.getRange('J2').setValue(valueE); //поменять ячейку для записи
+        sendData_({ cellA: valueA, cellE: valueE });
+      }
+    }
+  }
+}
+
+function sendData_(data) {
+  const url = `http://185.250.148.23:8888/sheet?cellA=${data.cellA}&cellE=${data.cellE}`;
+
+  const httpResponse = UrlFetchApp.fetch(url, {
+    muteHttpExeptions: true,
+  });
+
+  if (httpResponse.getResponseCode() === 200) {
+    console.log(httpResponse.getContentText());
+  } else {
+    console.error(httpResponse.getResponseCode(), httpResponse.getContentText());
+  }
+}
+
 function getData() {
   const table = SpreadsheetApp.getActive();
   const tableId = table.getId();
@@ -13,7 +60,7 @@ function getData() {
     },
     tableId,
   );
-  
+
   const sheet = response.sheets.find((s) => s.properties.title == sheetName);
   console.log(JSON.stringify(sheet.properties, null, 2));
   console.log('columns::', sheet.data[0].rowData[0].length);
